728a3659555db4e93b454717600672daa96354e1
Dushyant <dusht.ahuja@gmail.com>
2022-11-06 09:07:48 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,23 @@
 
 #include "Page_Admin.h"
 
+#define DEBUG
+
+#ifndef DEBUG_PRINT
+#ifdef DEBUG
+#define DEBUG_PRINT(x) Serial.println(x)
+#else
+#define DEBUG_PRINT(x)
+#endif
+#endif
+
 #define EN_PIN           D1 // Enable
 #define DIR_PIN          D3 // Direction
 #define STEP_PIN         D4 // Step
 constexpr uint32_t steps_per_mm = 80;
-uint maxSpeed = 14;
-uint accel = 7;
+uint8_t maxSpeed = 14;
+uint8_t accel = 7;
+uint8_t mode = 0; // 0 - smooth sea-saw, 1 - Random
 
 AccelStepper stepper = AccelStepper(stepper.DRIVER, STEP_PIN, DIR_PIN);
 int direction = 1;
@@ -31,29 +42,32 @@ AsyncWebServer httpServer(80);
 DNSServer dns;
 Espalexa espalexa;
 //fauxmoESP fauxmo;
-bool running = false;
+bool running = false, switchOn = false;
 void switchOnKinetic(uint8_t brightness);
 bool loadDefaultSettings();
 bool saveDefaultSettings();
 void printFile();
 
-unsigned long previousMillis = 0; 
+unsigned long previousMillis = 0, previousOnMillis = 0; 
 unsigned long interval = 1000;
 
 void setup() {
-    Serial.begin(74880);
+    #ifdef DEBUG
+        Serial.begin(74880);
+    #endif
 
     WiFi.setAutoConnect(true);
     WiFi.setSleepMode(WIFI_NONE_SLEEP);
     WiFi.persistent(true);
     if (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin("DUSHYANT", "ahuja987");
+      WiFi.begin("DUSHYANT-NEW", "ahuja987");
       while (WiFi.status() != WL_CONNECTED) {
-          Serial.print(".");
+          DEBUG_PRINT(".");
           delay(3000);
       }
     }
-    Serial.println("Wifi Setup Completed");
+    DEBUG_PRINT("Wifi Setup Completed");
+    DEBUG_PRINT(WiFi.localIP().toString());
     
     loadDefaultSettings();
 
@@ -65,11 +79,18 @@ void setup() {
     
     if (!SPIFFS.begin())
     {
-        Serial.println("An Error has occurred while mounting SPIFFS");
+        DEBUG_PRINT("An Error has occurred while mounting LittleFS");
         return;
     }
     httpServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
         AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/www/style.css.gz", "text/css");
@@ -88,41 +109,115 @@ void setup() {
     });
     httpServer.on("/ON", HTTP_GET, [](AsyncWebServerRequest *request) {
         running = true;
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        switchOn = true;
+        previousOnMillis = millis();
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/OFF", HTTP_GET, [](AsyncWebServerRequest *request) {
         running = false;
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        switchOn = false;
+        stepper.stop();
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/Increase", HTTP_GET, [](AsyncWebServerRequest *request) {
         maxSpeed++;
         stepper.setMaxSpeed(maxSpeed*steps_per_mm);
         saveDefaultSettings();
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/Decrease", HTTP_GET, [](AsyncWebServerRequest *request) {
         maxSpeed--;
         if (maxSpeed == 0) maxSpeed = 1;
         stepper.setMaxSpeed(maxSpeed*steps_per_mm);
         saveDefaultSettings();
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/IncAcc", HTTP_GET, [](AsyncWebServerRequest *request) {
         accel++;
         stepper.setAcceleration(accel*steps_per_mm);
         saveDefaultSettings();
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
+    });
+    httpServer.on("/Random", HTTP_GET, [](AsyncWebServerRequest *request) {
+        //accel++;
+        //stepper.setAcceleration(accel*steps_per_mm);
+        //saveDefaultSettings();
+        mode = 1;
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
+    });
+    httpServer.on("/Smooth", HTTP_GET, [](AsyncWebServerRequest *request) {
+        //accel++;
+        //stepper.setAcceleration(accel*steps_per_mm);
+        //saveDefaultSettings();
+        mode = 0;
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/DecAcc", HTTP_GET, [](AsyncWebServerRequest *request) {
         accel--; 
         if (accel ==0) accel = 1;
         stepper.setAcceleration(accel*steps_per_mm);
         saveDefaultSettings();
-        request->send_P(200, "text/html", PAGE_AdminMainPage);
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
+        message += String(accel)  + '\n';
+        message += "<BR>Max Speed: ";
+        message += String(maxSpeed)  + '\n';
+        message += "<BR>Stop Delay: ";
+        message += String(interval / 1000)  + '\n';
+        request->send(200, "text/html", message);
     });
     httpServer.on("/save", HTTP_GET, [](AsyncWebServerRequest *request) {
         if(!saveDefaultSettings()){
-            Serial.println("Unable to Save");
+            DEBUG_PRINT("Unable to Save");
             request->send_P(200, "text/plain", "Not Saved");
         }
         else
@@ -132,12 +227,12 @@ void setup() {
       if (!espalexa.handleAlexaApiCall(request)) //if you don't know the URI, ask espalexa whether it is an Alexa control request
       {
         //whatever you want to do with 404s
-        String message;
-        message = "Acceleration: ";
+        String message = PAGE_AdminMainPage;
+        message += "<HR><BR>Acceleration: ";
         message += String(accel)  + '\n';
-        message += "Max Speed: ";
+        message += "<BR>Max Speed: ";
         message += String(maxSpeed)  + '\n';
-        request->send(404, "text/plain", message);
+        request->send(200, "text/html", message);
       }
     });
     httpServer.addHandler(new SPIFFSEditor("admin", "admin"));
@@ -154,19 +249,38 @@ void loop() {
     if (WiFi.status() != WL_CONNECTED) {
       WiFi.begin("DUSHYANT", "ahuja987");
     }
-    if (stepper.distanceToGo() == 0) {
-        stepper.disableOutputs();
-        //delay(random(10,100)*100);
-        if(running && currentMillis - previousMillis >= interval) {
-            stepper.move(random(500)*steps_per_mm*direction); // Move 100mm
-            if(random(10)>=5) direction = -direction;
-            interval = random(10,100)*100;
-            previousMillis = currentMillis;
-            stepper.enableOutputs();
+    if(mode == 1){
+        if (stepper.distanceToGo() == 0) {
+            stepper.disableOutputs();
+            //delay(random(10,100)*100);
+            if(running && currentMillis - previousMillis >= interval) {
+                if(random(10)>=5) direction = -direction;
+                stepper.move(random(1000)*steps_per_mm*direction); 
+                interval = random(20,200)*100;
+                previousMillis = currentMillis;
+                stepper.enableOutputs();
+            }
         }
     }
+    else if(mode == 0){
+        if (stepper.distanceToGo() == 0) {
+            stepper.disableOutputs();
+            //delay(random(10,100)*100);
+            if(running && currentMillis - previousMillis >= interval) {
+                direction = -direction;
+                stepper.move(1000*steps_per_mm*direction); 
+                interval = 1000;
+                previousMillis = currentMillis;
+                stepper.enableOutputs();
+            }
+        }
+    }
+    if(currentMillis - previousOnMillis >= 300000 && switchOn){
+        running = !running;
+        previousOnMillis = currentMillis;
+    }
     stepper.run();
-    AsyncElegantOTA.loop();
+    //AsyncElegantOTA.loop();
     //fauxmo.handle();
     espalexa.loop();
 }
@@ -174,9 +288,12 @@ void loop() {
 void switchOnKinetic(uint8_t brightness) {
     if (brightness != 0) {
       running = true;
+      switchOn = true;
     }
     else {
       running = false;
+      switchOn = false;
+      stepper.stop();
     }
 }
 
@@ -184,19 +301,19 @@ bool loadDefaultSettings(){
     //if (!SPIFFS.exists("/config.json")) return false;
     if (!SPIFFS.begin())
     {
-        Serial.println("An Error has occurred while mounting SPIFFS");
+        DEBUG_PRINT("An Error has occurred while mounting SPIFFS");
         return false;
     }
     File  configFile  =  SPIFFS.open("config.json","r"); 
     if(!configFile)  { 
-        Serial.println( "Failed to open config file" ); 
+        DEBUG_PRINT( "Failed to open config file" ); 
         configFile.close();
         return  false ; 
     }
 
     size_t size  =  configFile.size(); 
     if(size > 256 )  { 
-        Serial.println( "Config file size is too large" ); 
+        DEBUG_PRINT( "Config file size is too large" ); 
         configFile.close();
         return  false ; 
     }
@@ -204,16 +321,16 @@ bool loadDefaultSettings(){
     DeserializationError error = deserializeJson(doc, configFile);
 
     if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
+        DEBUG_PRINT(F("deserializeJson() failed: "));
+        DEBUG_PRINT(error.f_str());
         configFile.close();
         return false;
     }
     serializeJsonPretty(doc,Serial);
     maxSpeed = doc["maxSpeed"];
     accel = doc["accel"];
-    Serial.println(maxSpeed);
-    Serial.println(accel);
+    DEBUG_PRINT(maxSpeed);
+    DEBUG_PRINT(accel);
     configFile.close();
     return true;
 }
@@ -224,7 +341,7 @@ bool saveDefaultSettings(){
     doc["accel"] = accel;
     File configFile  =  SPIFFS.open("config.json","w"); 
     if  ( !configFile )  { 
-        Serial.println( "Failed to open config file for writing" ); 
+        DEBUG_PRINT( "Failed to open config file for writing" ); 
         return  false ; 
     }
     serializeJson(doc, configFile);
@@ -238,15 +355,15 @@ void printFile() {
   // Open file for reading
   File file = SPIFFS.open("config.json","r"); 
   if (!file) {
-    Serial.println(F("Failed to read file"));
+    DEBUG_PRINT(F("Failed to read file"));
     return;
   }
 
   // Extract each characters by one by one
   while (file.available()) {
-    Serial.print((char)file.read());
+    DEBUG_PRINT((char)file.read());
   }
-  Serial.println();
+  DEBUG_PRINT();
 
   // Close the file
   file.close();
